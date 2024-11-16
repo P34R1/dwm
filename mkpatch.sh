d1f8b6c77979974cda79067722b9cc97fcfbfb13
pearl <undeadgamer279@gmail.com>
2024-11-15 22:33:51 -0500

    rename patch folder and script

--- mkpatch.sh
+++ mkpatch.sh
@@ -4,4 +4,4 @@
 read -p "Enter the name for the patch file (without extension): " inputtedText
 
 # Run git diff and redirect the output to the specified file
-git diff --cached > "patch/${inputtedText}.diff"
+git diff --cached > "patches/${inputtedText}.diff"
