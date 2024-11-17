a2569ce8e8583734bb674e3dfe87abbe3fedf0c1
pearl <undeadgamer279@gmail.com>
2024-11-16 22:09:43 -0500

    bar height

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 20;
 static const char *fonts[]          = { "JetBrainsMonoNerdFontPropo:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
